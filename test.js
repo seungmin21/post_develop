bd17e3f85fec1a49cf6b5a8d93960172911345cc
ohseungmin <ohmin2886@gmail.com>
2023-10-18 19:17:58 +0900

    POST와 GET 위치 변경

--- test.js
+++ test.js
@@ -3,19 +3,6 @@ const querystring = require('querystring');
 const fs = require('fs')
 
 http.createServer((req, res) => {
-  if (req.method === 'POST' && req.url === '/sub') {
-    let body = '';
-    req.on('data', chunk => {
-      body += chunk, toString()
-    })
-    req.on('end', () => {
-      const parseBody = querystring.parse(body)
-      const { userName, passWard } = parseBody
-
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end("hello")
-    })
-  } 
   if (req.method === 'GET' && req.url === '/') {
       fs.readFile('index.html', 'utf8', (err, data) => {
         if(err) {
@@ -26,4 +13,24 @@ http.createServer((req, res) => {
         }
       })
     }
-}).listen(3000)
+    if (req.method === 'POST' && req.url === '/login') {
+      let body = '';
+      req.on('data', (chunk) => {
+        body += chunk.toString()
+      })
+      req.on('end', () => {
+        const parseBody = querystring.parse(body)
+        const { userName, passWord } = parseBody
+        
+        console.log(`form`, parseBody)
+        console.log(`form`, passWord)
+        console.log(`form`, userName)
+  
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end("hello")
+      })
+    } 
+}).listen(3000, () => {
+  console.log(`http://localhost:${PORT}/`)
+})
+let PORT = 3000;
