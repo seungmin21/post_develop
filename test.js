fe0c36c3d433ba7bb52b88112a08fa6fcbce3566
ohseungmin <ohmin2886@gmail.com>
2023-10-18 16:55:05 +0900

    post로 데이터 요청 GET으로 응답

---
+++ test.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const querystring = require('querystring');
+
+http.createServer((req, res) => {
+  if (req.method === 'POST' && req.url === '/sub') {
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk, toString()
+    })
+    req.on('end', () => {
+      const parseBody = querystring.parse(body)
+      const { userName, passWard } = parseBody
+    })
+  } if (req.method === 'GET' && req.url === '/') {
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.end("");
+} 
+}).listen(3000)
