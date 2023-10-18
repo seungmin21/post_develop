e9de1181bc70c2491cf91b243b6533aa864df482
ohseungmin <ohmin2886@gmail.com>
2023-10-18 18:21:24 +0900

    POST 응답 작성 및 GET 파일 읽기 후 응답 작성

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const querystring = require('querystring');
+const fs = require('fs')
 
 http.createServer((req, res) => {
   if (req.method === 'POST' && req.url === '/sub') {
@@ -10,9 +11,19 @@ http.createServer((req, res) => {
     req.on('end', () => {
       const parseBody = querystring.parse(body)
       const { userName, passWard } = parseBody
+
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end("hello")
     })
-  } if (req.method === 'GET' && req.url === '/') {
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    res.end("");
-} 
+  } 
+  if (req.method === 'GET' && req.url === '/') {
+      fs.readFile('index.html', 'utf8', (err, data) => {
+        if(err) {
+          console.log(err)
+        } else {
+          res.writeHead(200, {'Content-Type': 'text/html'})
+          res.end(data);
+        }
+      })
+    }
 }).listen(3000)
