d827049a68be5478eef1dd28591f44a5a3ec6dcd
ohseungmin <ohmin2886@gmail.com>
2023-10-18 16:43:15 +0900

    POST 작성

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('hello');
+const http = require('http');
+const querystring = require('querystring');
+
+http.createServer((req, res) => {
+  let body = '';
+  if (req.method === 'POST' && req.url === '/') {
+    req.on('data', chunk => {
+      body += chunk, toString()
+    })
+    req.on('end', () => {
+      const parseBody = querystring.parse(body)
+      const { userName, passWard } = parseBody
+
+      res.writeHead(200, {'Content-Type': 'text/html'})
+      res.end("");
+    })
+  }
+}).listen(3000)
